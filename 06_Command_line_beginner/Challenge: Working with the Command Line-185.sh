cf29d69841b84163bc65036dcbf26031a762681e
anirtek <atekade1@binghamton.edu>
2018-03-04 14:20:27 -0500

    Command Line beginner

---
+++ 06_Command_line_beginner/Challenge: Working with the Command Line-185.sh
@@ -0,0 +1,19 @@
+## 1. Introduction ##
+
+/home/dq$ pwd
+
+## 2. Create a Script ##
+
+/home/dq$ nano script.py
+
+## 3. Change File Permissions ##
+
+/home/dq$ chmod 0700 script.py
+
+## 5. Move the Script ##
+
+(script) /home/dq$ mv script.py printer/.
+
+## 6. Execute the Script ##
+
+(script) /home/dq/printer$ python script.py "I'm so good at challenges!"
